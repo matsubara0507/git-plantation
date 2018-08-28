a071fd7eb34625c92aaca3d098a49ba895f69c22
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-08-29 01:05:59 +0900

    Feat: score api

--- src/Git/Plantation/API/CRUD.hs
+++ src/Git/Plantation/API/CRUD.hs
@@ -5,12 +5,19 @@
 module Git.Plantation.API.CRUD where
 
 import           RIO
+import qualified RIO.Map                as Map
 
+import           Data.Default.Class
 import           Data.Extensible
+import qualified Drone.Client           as Drone
+import qualified Drone.Endpoints        as Drone
+import qualified Drone.Types            as Drone
+import           Git.Plantation.Cmd     (splitRepoName)
 import           Git.Plantation.Env     (Plant)
 import           Git.Plantation.Problem (Problem)
-import           Git.Plantation.Score   (Score)
+import           Git.Plantation.Score   (Score, Status)
 import           Git.Plantation.Team    (Team)
+import           Network.HTTP.Req
 import           Servant
 
 type CRUD
@@ -31,15 +38,39 @@ getProblems :: Plant [Problem]
 getProblems = asks (view #problems . view #config)
 
 getScores :: Plant [Score]
-getScores = map toScore <$> getTeams
+getScores = do
+  teams    <- getTeams
+  problems <- getProblems
+  let client = Drone.HttpsClient $ #host @= "localhost" <: #token @= "" <: nil
+  builds <- Map.fromList <$> mapM (fetchBuilds client) problems
+  pure $ map (mkScore problems builds) teams
 
--- ToDo
-toScore :: Team -> Score
-toScore team
-    = #team  @= (team ^. #name)
-   <: #point @= 0
-   <: #stats @= [ stat1, stat2 ]
+fetchBuilds :: Drone.Client c => c -> Problem -> Plant (Text, [Drone.Build])
+fetchBuilds client problem = do
+  let (owner, repo) = splitRepoName $ problem ^. #repo_name
+  builds <- responseBody <$> runReq def (Drone.getBuilds client owner repo)
+  pure (problem ^. #problem_name, builds)
+
+mkScore :: [Problem] -> Map Text [Drone.Build] -> Team -> Score
+mkScore problems builds team
+    = #team  @= team ^. #name
+   <: #point @= sum (map (toPoint stats) problems)
+   <: #stats @= stats
    <: nil
-   where
-     stat1 = #problem @= "tutorial"    <: #correct @= True  <: nil
-     stat2 = #problem @= "minesweeper" <: #correct @= False <: nil
+  where
+    isTeamBuild b = b ^. #branch == team ^. #name
+    builds' = Map.filter (not . null) $ Map.map (filter isTeamBuild) builds
+    stats = Map.elems $ Map.mapWithKey toStatus builds'
+
+toStatus :: Text -> [Drone.Build] -> Status
+toStatus name builds
+    = #problem @= name
+   <: #correct @= any (\b -> b ^. #status == "success") builds
+   <: nil
+
+toPoint :: [Status] -> Problem -> Int
+toPoint stats problem =
+  if elem (problem ^. #problem_name) $ map (view #problem) stats then
+    problem ^. #difficulty
+  else
+    0
