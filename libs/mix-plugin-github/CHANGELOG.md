07710a2f830e902b73fd25ca08b7818a65bdce5c
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-04-08 20:53:14 +0900

    Add mix plugin for github

---
+++ libs/mix-plugin-github/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog for mix-plugin-github
+
+## Unreleased changes
---
+++ libs/mix-plugin-github/src/Mix/Plugin/GitHub.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeOperators    #-}
+
+module Mix.Plugin.GitHub
+  ( HasGitHubToken (..)
+  , buildPlugin
+  , tokenText
+  , auth
+  , fetch
+  ) where
+
+import           RIO
+
+import           Data.Extensible
+import qualified GitHub.Auth     as GitHub
+import           Mix.Plugin      (Plugin, toPlugin)
+
+buildPlugin :: MonadIO m => GitHub.Token -> Plugin a m GitHub.Token
+buildPlugin token = toPlugin $ \f -> f token
+
+class HasGitHubToken env where
+  tokenL :: Lens' env GitHub.Token
+
+instance Associate "github" GitHub.Token xs => HasGitHubToken (Record xs) where
+  tokenL = lens (view #github) (\x y -> x & #github `set` y)
+
+tokenText :: (MonadIO m, MonadReader env m, HasGitHubToken env) => m Text
+tokenText = tshow <$> view tokenL
+
+auth :: (MonadIO m, MonadReader env m, HasGitHubToken env) => m GitHub.Auth
+auth = GitHub.OAuth <$> view tokenL
+
+fetch :: (MonadIO m, MonadReader env m, HasGitHubToken env) => (GitHub.Auth -> m a) -> m a
+fetch act = act =<< auth
