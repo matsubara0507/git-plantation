a48c0e669bc30fa241ac661635488378342ac85c
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-05-20 09:31:08 +0900

    Fix: thread unsafe with shelly

--- libs/shh-cmd/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelog for shh-cmd
-
-## Unreleased changes
---
+++ src/Git/Cmd.hs
@@ -0,0 +1,45 @@
+module Git.Cmd where
+
+import           RIO
+import qualified RIO.Text as Text
+
+import           Shelly   hiding (FilePath, unlessM)
+
+clone :: [Text] -> Sh ()
+clone = command1_ "git" [] "clone"
+
+fetch :: [Text] -> Sh ()
+fetch = command1_ "git" [] "fetch"
+
+pull :: [Text] -> Sh ()
+pull = command1_ "git" [] "pull"
+
+remote :: [Text] -> Sh ()
+remote = command1_ "git" [] "remote"
+
+push :: [Text] -> Sh ()
+push = command1_ "git" [] "push"
+
+checkout :: [Text] -> Sh ()
+checkout = command1_ "git" [] "checkout"
+
+commit :: [Text] -> Sh ()
+commit = command1_ "git" [] "commit"
+
+add :: [Text] -> Sh ()
+add = command1_ "git" [] "add"
+
+branch :: [Text] -> Sh ()
+branch = command1_ "git" [] "branch"
+
+cloneOrFetch :: Text -> Text -> Sh ()
+cloneOrFetch repoUrl repoName = do
+  dir <- pwd
+  let repoDir = dir </> repoName
+  unlessM (test_d repoDir) $ clone [repoUrl, repoName]
+  chdir repoDir $ fetch ["origin"]
+
+existBranch :: Text -> Sh Bool
+existBranch branchName = do
+  branches <- Text.lines <$> command1 "git" [] "branch" []
+  pure $ any (Text.isSuffixOf branchName) branches
