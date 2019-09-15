8872f170150338e472828a4ebfc261dc8aa9b0b4
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-09-16 01:42:18 +0900

    Refactor: update to lts-14.6

--- src/Orphans.hs
+++ src/Orphans.hs
@@ -5,17 +5,15 @@
 module Orphans () where
 
 import           RIO
-import qualified RIO.Text           as Text
 
 import           Data.Extensible
-import           GHC.TypeLits       (symbolVal)
 import           Web.FormUrlEncoded
 import           Web.HttpApiData
 
-instance Forall (KeyValue KnownSymbol FromHttpApiData) xs => FromForm (Record xs) where
+instance Forall (KeyTargetAre KnownSymbol FromHttpApiData) xs => FromForm (Record xs) where
   fromForm form =
-    hgenerateFor (Proxy @ (KeyValue KnownSymbol FromHttpApiData)) $ \m ->
-      let k = Text.pack $ symbolVal (proxyAssocKey m) in Field <$> parseUnique k form
+    hgenerateFor (Proxy @ (KeyTargetAre KnownSymbol FromHttpApiData)) $ \m ->
+      Field <$> parseUnique (stringKeyOf m) form
 
 instance FromHttpApiData a => FromHttpApiData (Identity a) where
   parseUrlPiece = fmap pure . parseUrlPiece
